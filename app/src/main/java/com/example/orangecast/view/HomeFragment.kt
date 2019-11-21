d222c3ffc02d2e327451fa3701836249d7834d63
Arman <armanisnumerouno@gmail.com>
2019-11-21 18:08:00 +0600

    SearchViewModel cannot create an instance

--- app/src/main/java/com/example/orangecast/view/HomeFragment.kt
+++ app/src/main/java/com/example/orangecast/view/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orangecast.R
@@ -19,38 +20,38 @@ import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private var searchViewModel: SearchViewModel? = null
+    private lateinit var searchViewModel: SearchViewModel
     private var adapter: MediaItemsAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        searchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
         adapter = MediaItemsAdapter()
         list_rv?.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         list_rv?.adapter = adapter
 
-        searchViewModel?.getSearchResult()?.observe(viewLifecycleOwner, Observer { event ->
+        searchViewModel.getSearchResult().observe(viewLifecycleOwner, Observer { event ->
             adapter?.setItems(event?.results ?: listOf())
         })
 
         search_view?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return if (query != null) {
-                    searchViewModel?.search()
+                    searchViewModel.search()
                     true
                 } else false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
                 return if (newText != null) {
-                    searchViewModel?.searchingText = newText
+                    searchViewModel.searchingText = newText
                     true
                 } else false
             }
