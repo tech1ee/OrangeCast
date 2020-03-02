6fa405e4e7676cda8e715215b4a7d2e2a5bc0601
Arman <armanisnumerouno@gmail.com>
2020-03-02 20:35:56 +0600

    channel details layouts, Fragment, RV Adapter

---
+++ app/src/main/java/com/example/orangecast/view/channeldetails/ChannelDetailsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.orangecast.view.channeldetails
+
+import com.example.orangecast.view.BaseFragment
+
+class ChannelDetailsFragment: BaseFragment() {
+
+    override fun inject() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/orangecast/view/episodes/EpisodesAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.orangecast.view.episodes
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orangecast.R
+import com.example.orangecast.data.MediaItem
+import kotlinx.android.synthetic.main.item_media_item.view.*
+
+class EpisodesAdapter(
+    private val listener: Listener
+): RecyclerView.Adapter<EpisodesAdapter.ViewHolder>() {
+
+    private val list = arrayListOf<MediaItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_media_item, parent, false))
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(position)
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val episodeNumber = view.episode_number
+        private val publishingDate = view.publishing_date
+        private val episodeTitle = view.episode_title
+        private val episodeDuration = view.episode_duration
+        private val listeningProgressView = view.listening_progress_view
+        private val listeningProgress = view.listening_progress
+        private val playButton = view.play_button
+
+        fun bind(position: Int) {
+            val item = list[position]
+
+        }
+    }
+
+    interface Listener {
+
+    }
+}
